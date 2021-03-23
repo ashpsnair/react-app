4b6acd5a609d30325d9787e1c2c373132a489caf
Aiswarya P.S <69599173+ashpsnair@users.noreply.github.com>
2021-03-23 23:40:57 +0530

    initial commit

---
+++ devchallenges1-button/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+      < Button name="default" />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ devchallenges1-button/src/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Button.css';
+
+function Button(props) {
+    return (
+        
+        <div>
+            <button className={props.name}> Default</button>
+            
+        </div>      
+        
+    );
+}
+
+export default Button;
